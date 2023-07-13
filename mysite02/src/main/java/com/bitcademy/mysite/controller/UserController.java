eacad79b08d6cd0e86ee85244cee1feabdd734a4
비트부트캠프 <137239441+bitacademy-bootcamp23-backend@users.noreply.github.com>
2023-07-13 16:33:43 +0900

    Update UserController.java

--- mysite02/src/main/java/com/bitcademy/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/bitcademy/mysite/controller/UserController.java
@@ -89,7 +89,31 @@ public class UserController extends HttpServlet {
 			request
 				.getRequestDispatcher("/WEB-INF/views/user/updateform.jsp")
 				.forward(request, response);				
-		}
+		} else if("update".equals(actionName)) {
+			// Access Control(보안, 인증체크)
+			HttpSession session = request.getSession();
+			UserVo authUser = (UserVo)session.getAttribute("authUser");
+			if(authUser == null) {
+				MvcUtil.redirect(request.getContextPath(), request, response);
+				return;
+			}
+			//////////////////////////////////////////////////////
+			
+			String name = request.getParameter("name");
+			String password = request.getParameter("password");
+			String gender = request.getParameter("gender");
+			
+			UserVo vo = new UserVo();
+			vo.setName(name);
+			vo.setPassword(password);
+			vo.setGender(gender);
+			vo.setNo(authUser.getNo());
+	
+			new UserDao().update(vo);
+			authUser.setName(name);
+			
+			MvcUtil.redirect(request.getContextPath() + "/user?a=updateform", request, response);
+		} 
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
