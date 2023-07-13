4972d9ef5fd6f50f90d31ce3d9ca2328e0cd9284
비트부트캠프 <137239441+bitacademy-bootcamp23-backend@users.noreply.github.com>
2023-07-13 16:28:12 +0900

    Update GuestbookController.java

--- mysite02/src/main/java/com/bitcademy/mysite/controller/GuestbookController.java
+++ mysite02/src/main/java/com/bitcademy/mysite/controller/GuestbookController.java
@@ -11,6 +11,35 @@ public class GuestbookController extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
+		String actionName = request.getParameter("a");
+
+		if("delete".equals(actionName)) {
+			String sno = request.getParameter("no");
+			Long no = Long.parseLong(sno);
+			String password = request.getParameter("password");
+			
+			new GuestbookDao().deleteByNoAndPassword(no, password);
+			MvcUtil.redirect(request.getContextPath() + "/guestbook", request, response);	
+		} else if("deleteform".equals(actionName)) {
+			MvcUtil.forward("guestbook/deleteform", request, response);
+		} else if("add".equals(actionName)) {
+			String name = request.getParameter("name");
+			String password = request.getParameter("password");
+			String message = request.getParameter("message");
+			
+			GuestbookVo vo = new GuestbookVo();
+			vo.setName(name);
+			vo.setPassword(password);
+			vo.setMessage(message);
+			
+			new GuestbookDao().insert(vo);
+			response.sendRedirect(request.getContextPath() + "/guestbook");		
+		} else {
+			List<GuestbookVo> list = new GuestbookDao().findAll();
+		
+			request.setAttribute("list", list);
+			MvcUtil.forward("guestbook/list", request, response);
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
