f16d00544a413f67db3278ef47f55dbc0f7e7cd6
BIT <BIT@DESKTOP-EJDVL85>
2023-07-13 13:27:37 +0900

    cookie 연습

--- mysite02/src/main/java/com/bitcademy/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/bitcademy/mysite/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.bitcademy.mysite.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.bitcademy.mysite.dao.UserDao;
+import com.bitcademy.mysite.vo.UserVo;
 
 public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -22,10 +26,41 @@ public class UserController extends HttpServlet {
 				.getRequestDispatcher("/WEB-INF/views/user/joinsuccess.jsp")
 				.forward(request, response);
 		} else if("join".equals(actionName)) {
-			// 파라미터 뽑아내기
-			// 다오.insert(vo)
-
+			String name = request.getParameter("name");
+			String email = request.getParameter("email");
+			String password = request.getParameter("password");
+			String gender = request.getParameter("gender");
+			
+			UserVo vo = new UserVo();
+			vo.setName(name);
+			vo.setEmail(email);
+			vo.setPassword(password);
+			vo.setGender(gender);
+			
+			new UserDao().insert(vo);
+			
 			response.sendRedirect(request.getContextPath() + "/user?a=joinsuccess");
+		} else if("loginform".equals(actionName)) {
+			request
+				.getRequestDispatcher("/WEB-INF/views/user/loginform.jsp")
+				.forward(request, response);
+		} else if("login".equals(actionName)) {
+			String email = request.getParameter("email");
+			String password = request.getParameter("password");
+			
+			UserVo authUser = new UserDao().findByEmailAndPassword(email, password);
+			if(authUser == null) {
+				// 인증 실패
+				// response.sendRedirect(request.getContextPath() + "/user?a=loginform&result=fail");
+				request.setAttribute("result", "fail");
+				request
+					.getRequestDispatcher("/WEB-INF/views/user/loginform.jsp")
+					.forward(request, response);				
+				return;
+			}
+			
+			// 인증 성공
+			System.out.println("로그인 처리");
 		}
 	}
 
