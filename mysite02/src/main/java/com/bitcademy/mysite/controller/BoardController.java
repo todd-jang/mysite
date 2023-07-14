c3cfcc320b930bea6f4fdb4ec877d798a611252d
BIT <BIT@DESKTOP-EJDVL85>
2023-07-14 15:09:48 +0900

    게시판 1

---
+++ mysite02/src/main/java/com/bitcademy/mysite/controller/BoardController.java
@@ -0,0 +1,34 @@
+package com.bitcademy.mysite.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BoardController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		
+		String actionName = request.getParameter("a");
+		if("writeform".equals(actionName)) {
+			
+		} else if("write".equals(actionName)) {
+			
+		} else {
+			/* default action */
+			
+			
+			request
+				.getRequestDispatcher("/WEB-INF/views/board/list.jsp")
+				.forward(request, response);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
