7389022c66be5633fb641b25cc18fb9175e4b234
비트부트캠프 <137239441+bitacademy-bootcamp23-backend@users.noreply.github.com>
2023-07-19 16:55:43 +0900

    Add files via upload

--- mysite03/src/main/java/com/bitacademy/mysite/service/GuestbookService.java
+++ mysite03/src/main/java/com/bitacademy/mysite/service/GuestbookService.java
@@ -1,19 +1,35 @@
-package com.bitacademy.mysite.service;
+package com.douzone.mysite.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.douzone.mysite.repository.GuestbookRepository;
+import com.douzone.mysite.vo.GuestbookVo;
 
 @Service
 public class GuestbookService {
+	@Autowired
+	private GuestbookRepository guestbookRepository;
 	
 	public List<GuestbookVo> getMessageList() {
-		return null;
+			return guestbookRepository.findAll();
 	}
 	
-	public void deleteMessage(Long no, String password) {
+	public List<GuestbookVo> getMessageList(Long no) {
+		return guestbookRepository.findAll(no);
+	}	
+	
+	public Boolean deleteMessage(Long no, String password) {
+		GuestbookVo vo = new GuestbookVo();
+		vo.setNo(no);
+		vo.setPassword(password);
+		
+		return 1 == guestbookRepository.deleteByNoAndPassword(no, password);
 	}
 
 	public void addMessage(GuestbookVo vo) {
+		guestbookRepository.insert(vo);
 	}
 }
