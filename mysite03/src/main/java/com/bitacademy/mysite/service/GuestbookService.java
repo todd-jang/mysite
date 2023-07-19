332e40aabdfef30751084b70dd9e3ce5a343a3d9
BIT <BIT@DESKTOP-EJDVL85>
2023-07-19 15:56:13 +0900

    mysite - guestbook I

---
+++ mysite03/src/main/java/com/bitacademy/mysite/service/GuestbookService.java
@@ -0,0 +1,19 @@
+package com.bitacademy.mysite.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GuestbookService {
+	
+	public List<GuestbookVo> getMessageList() {
+		return null;
+	}
+	
+	public void deleteMessage(Long no, String password) {
+	}
+
+	public void addMessage(GuestbookVo vo) {
+	}
+}
