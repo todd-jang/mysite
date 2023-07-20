2c105623955c8fe71d634565a9d3a99244170ee6
BIT <BIT@DESKTOP-EJDVL85>
2023-07-20 11:51:59 +0900

    mysite security

--- mysite03/src/main/java/com/bitacademy/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/bitacademy/mysite/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.bitacademy.mysite.controller;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.bitacademy.mysite.security.Auth;
+import com.bitacademy.mysite.security.AuthUser;
 import com.bitacademy.mysite.service.UserService;
 import com.bitacademy.mysite.vo.UserVo;
 
@@ -38,46 +37,10 @@ public class UserController {
 	public String login() {
 		return "user/login";
 	}
-
-	@RequestMapping(value="/login", method=RequestMethod.POST)
-	public String login(HttpSession session, UserVo vo, Model model) {
-		UserVo authUser = userService.getUser(vo);
-		if(authUser == null) {
-			model.addAttribute("result", "fail");
-			return "user/login";
-		}
-		
-		/* 로그인 처리 */
-		session.setAttribute("authUser", authUser);
-		
-		return "redirect:/";
-	}
-	
-	@RequestMapping("/logout")
-	public String logout(HttpSession session) {
-		// 접근 제어
-		UserVo authUser = (UserVo)session.getAttribute("authUser");
-		if(authUser == null) {
-			return "redirect:/";
-		}
-		////////////////////////
-		
-		/* 로그아웃 처리 */
-		session.removeAttribute("authUser");
-		session.invalidate();
-		return "redirect:/";
-	}
 	
 	@Auth
 	@RequestMapping(value="/update", method=RequestMethod.GET)
 	public String update(@AuthUser UserVo authUser, Model model) {
-		// 접근 제어
-		// UserVo authUser = (UserVo)session.getAttribute("authUser");
-		// if(authUser == null) {
-		//	return "redirect:/";
-		// }
-		////////////////////////		
-		
 		Long no = authUser.getNo();
 		UserVo userVo = userService.getUser(no);
 		
@@ -85,15 +48,9 @@ public class UserController {
 		return "user/update";
 	}
 
+	@Auth
 	@RequestMapping(value="/update", method=RequestMethod.POST)
-	public String update(HttpSession session, UserVo vo) {
-		// 접근 제어
-		UserVo authUser = (UserVo)session.getAttribute("authUser");
-		if(authUser == null) {
-			return "redirect:/";
-		}
-		////////////////////////		
-		
+	public String update(@AuthUser UserVo authUser, UserVo vo) {
 		vo.setNo(authUser.getNo());
 		userService.updateUser(vo);
 		
