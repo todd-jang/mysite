92a7df8a3c619c8d2484aae41c2fbb1ac5f63573
BIT <BIT@DESKTOP-EJDVL85>
2023-07-20 15:54:09 +0900

    mybatis #finish

--- mysite03/src/main/java/com/bitacademy/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/com/bitacademy/mysite/repository/GuestbookRepository.java
@@ -1,60 +1,25 @@
 package com.bitacademy.mysite.repository;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
-import javax.sql.DataSource;
-
+import java.util.Map;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
 import com.bitacademy.mysite.vo.GuestbookVo;
 
 @Repository
 public class GuestbookRepository {
-	@Autowired
-	private DataSource dataSource;
-
 	@Autowired
 	private SqlSession sqlSession;
 	
 	public Boolean deleteByNoAndPassword(Long no, String password) {
 		boolean result = false;
 		
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		
-		try {
-			conn = dataSource.getConnection();
-			
-			String sql = "delete from guestbook where no = ? and password = ?";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setLong(1, no);
-			pstmt.setString(2, password);
-			
-			int count = pstmt.executeUpdate();
-			
-			result = count == 1;
-		} catch (SQLException e) {
-			System.out.println("Error:" + e);
-		} finally {
-			try {
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				
-				if(conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
+		Map<String, Object> map = new HashMap<>();
+		map.put("no", no);
+		map.put("password", password);
+		sqlSession.delete("guestbook.deleteByNoAndPassword", map);
 		
 		return result;		
 	}
