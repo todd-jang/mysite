6aeebcacbe0076432f78facf32f4261a1b404520
bitacademy-bootcamp23-backend <bitacademy.bootcamp23.backend@gmail.com>
2023-07-26 11:36:09 +0900

    404 예외 처리

--- mysite05/src/main/java/com/bitacademy/mysite/exception/GlobalExceptionHandler.java
+++ mysite05/src/main/java/com/bitacademy/mysite/exception/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,6 +16,11 @@ public class GlobalExceptionHandler {
 	
 	@ExceptionHandler(Exception.class)
 	public String handlerException(Model model, Exception ex) {
+		//1. 404 Error 처리
+		if(ex instanceof NoHandlerFoundException) {
+			return "error/404";
+		}
+		
 		//1. 로깅
 		StringWriter errors = new StringWriter();
 		ex.printStackTrace(new PrintWriter(errors));
